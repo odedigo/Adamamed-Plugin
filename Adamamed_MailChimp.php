fcbd281fac5376e429c0e5267934c9e0b4df9b19
Oded <oded.cnaan@gmail.com>
2018-01-07 12:44:38 +0200

    mail chimp integration

--- Adamamed_MailChimp.php
+++ Adamamed_MailChimp.php
@@ -2,7 +2,8 @@
 
 class Adamamed_MailChimp {
 
-    protected $mailChimp_form_id = 4502;
+    protected $mailChimp_form_id = 3872;
+    protected $fields = array('email'=>'your-email','name'=>'your-name');
 
     public function beforeFormSent($form_tag) {
         $form = WPCF7_Submission::get_instance();
@@ -30,15 +31,109 @@ class Adamamed_MailChimp {
             }
 
             $d = print_r($form_data,true);
-            $name = $form_data['your-name'];
-            $email = $form_data['your-email'];
+            $name = $form_data[$this->fields['name']];
+            $email = $form_data[$this->fields['email']];
 
-            $mail = $form_tag->prop( 'mail' ); // returns array 
-            // add content to email body
-            $mail['body'] .= 'Data:<br>'.$name." [".$email."]";
-            // set mail property with changed value(s)
-            $form_tag->set_properties( array( 'mail' => $mail ) );
+            $all_names = $this->getFirstFamilityName($name);
+            if ($all_names == null)
+                return;
+
+            $msg = 'Not submitted';
+            if(!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+                $msg = $this->sumbitToMailChimp($all_names['fname'],$all_names['lname'],$email);
+            }
         }
+    }
+
+    protected function sumbitToMailChimp($first_name, $last_name, $email) {
+        // MailChimp API credentials
+        $apiKey = '4f0e7517c6913ecd164fc0567a08d83c-us9';
+        $listID = '96fda3d4c8';
+
+        // MailChimp API URL
+        $memberID = md5(strtolower($email));
+        $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
+        $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listID . '/members/' . $memberID;
+
+        // member information
+        $json = json_encode([
+            'email_address' => $email,
+            'status'        => 'subscribed',
+            'merge_fields'  => [
+                'FNAME'     => $first_name,
+                'LNAME'     => $last_name
+            ]
+        ]);
+
+        // send a HTTP POST request with curl
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $apiKey);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        $result = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        // store the status message based on response code
+        if ($httpCode == 200) {
+            $msg = '<p style="color: #34A853">You have successfully subscribed to CodexWorld.</p>';
+        } else {
+            switch ($httpCode) {
+                case 214:
+                    $msg = 'You are already subscribed.';
+                    break;
+                default:
+                    $msg = 'Some problem occurred, please try again.';
+                    break;
+            }
+            $msg = '<p style="color: #EA4335">'.$msg.'</p>';
+        }
+        return $msg;
+    }
+
+    protected function getFirstFamilityName($form_name) {
+        $names = explode(' ',$form_name);
+        $the_names = array();
+        if (sizeof($names) == 0) {
+            return null;
+        }
+        else if (sizeof($names) == 1) {
+            $the_names['fname'] = $names[0];
+            $the_names['lname'] = $names[0]; // use first name as last name
+            return $the_names;
+        }
+        else if (sizeof($names) == 2) {
+            $the_names['fname'] = $names[0];
+            $the_names['lname'] = $names[1];
+            return $the_names;
+        }
+        $isFirst = true;
+        // first name is the first item
+        $the_names['fname'] = $names[0];
+        $fname = '';
+        foreach ($names as $item) {
+            if ($isFirst) {
+                $isFirst = false;
+            }
+            else {
+                $fname .= $item." ";
+            }
+        }
+        // last name is all the rest
+        $the_names['lname'] = $fname;
+        return $the_names;    
+    }
+
+    protected function addMessageToBody($form_tag, $msg) {
+        $mail = $form_tag->prop( 'mail' ); // returns array 
+        // add content to email body
+        $mail['body'] = 'Message: '.$msg. "\r\n".$mail['body'];
+        // set mail property with changed value(s)
+        $form_tag->set_properties( array( 'mail' => $mail ) );
     }
 
 }
