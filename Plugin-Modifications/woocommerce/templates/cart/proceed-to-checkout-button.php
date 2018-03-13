2b62c161da435f86a92ff4a879e7a661a52f01f0
Oded Cnaan <oded.cnaan@gmail.com>
2018-03-13 11:03:58 +0200

    plugin backup

---
+++ Plugin-Modifications/woocommerce/templates/cart/proceed-to-checkout-button.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Proceed to checkout button
+ *
+ * Contains the markup for the proceed to checkout button on the cart.
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/cart/proceed-to-checkout-button.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 2.4.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+<!-- START ODED 
+    CHANGE: changed the URL
+    DESC: Displays the details form before final checkout
+-->
+<!--<a href="<?php echo esc_url( wc_get_checkout_url() );?>" class="checkout-button button alt wc-forward"> -->
+<a href="<?php echo esc_url( get_permalink(3857));?>" class="checkout-button button alt wc-forward">
+  <?php esc_html_e( 'Proceed to checkout', 'woocommerce' ); ?>
+</a>
+<!-- END ODED -->
